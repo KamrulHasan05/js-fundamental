2ab5209eaef222ec2d7b2155fc98af249f7818ac
Kamrul Hasan <mdkamrulhasan.du@gmail.com>
2022-03-06 19:43:16 +0600

    done

--- jsSimpleBonus/factorialRecursion.js
+++ jsSimpleBonus/factorialRecursion.js
@@ -6,7 +6,7 @@ function factorial(number) {
     return factor;
 }
 
-console.log(factorial(7));
+// console.log(factorial(7));
 
 function factorialRecursion(number) {
     if (number == 1) {
@@ -14,4 +14,39 @@ function factorialRecursion(number) {
     }
     return number * factorialRecursion(--number);
 }
-console.log(factorialRecursion(7))
+// console.log(factorialRecursion(7))
+
+
+const factRecurtion = (i) => {
+    if (i == 1) {
+        return 1;
+    }
+    return i * factRecurtion(--i)
+}
+// console.log(factRecurtion(5));
+
+
+const sentence = 'hello, how are you'
+let reverse = ''
+for (let i = 0; i < sentence.length; i++) {
+    reverse = sentence[i] + reverse
+}
+// console.log(reverse);
+
+const strReverse = sentence => {
+    let reverse = ''
+    for (const character of sentence) {
+        reverse = character + reverse
+    }
+    return reverse
+}
+// console.log(strReverse(sentence));
+
+
+const factorilaRecursion = number => {
+    if (number == 1) {
+        return 1;
+    }
+    return number * factorialRecursion(--number)
+}
+// console.log(factorialRecursion(5));
